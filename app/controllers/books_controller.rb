f19d595dac96a7796105bda43771355f1ace3dc3
albert <albert3787@gmail.com>
2022-04-16 22:51:57 +0800

    first commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,9 @@
 class BooksController < ActionController::Base
     def index
     end
+
+    def create
+        book = Book.new
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/books",to "books#index"
-
-  roots "books#index"
+  get "/books",to: "books#index"
+  get "/books/new", to: "books#create"
+  root "books#index"
+  
 end
